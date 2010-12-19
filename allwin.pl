d4f30e4b1d511d28b9ce49941ae13f2440394620
horgh <will@summercat.com>
2010-12-18 18:03:59 -0800

    change actions to display as action in allwin rather than as regular message

--- allwin.pl
+++ allwin.pl
@@ -26,7 +26,7 @@ use Irssi;
 use vars qw($VERSION %IRSSI); 
 use POSIX;
 
-$VERSION = "20100825";
+$VERSION = "20101218";
 %IRSSI = (
 	authors     => "horgh",
 	contact     => "will\@summercat.com", 
@@ -43,7 +43,8 @@ Irssi::theme_register([
 	# $0 = nick, $1 = chan, $2 = msg, $3 = timestamp
 # This shows in format <nick:#chan> or the like
 #	'allmsg', '$3{pubmsgnick $0 {msgchannel $1}}$2'
-	'allmsg', '$3 $1 {pubmsgnick $0}$2'
+	'allmsg', '$3 $1 {pubmsgnick $0}$2',
+	'allmsg_action', '$3 $1 {pubaction $0}$2'
 ]);
 
 my %session_colours = {};
@@ -77,8 +78,8 @@ sub format_channel {
 	return chr(3) . $colour . $channel;
 }
 
-sub sig_msg_pub {
-	my ($server, $msg, $nick, $address, $target) = @_;
+sub window_output {
+	my ($format, $nick, $target, $msg) = @_;
 	$target = lc($target);
 
 	# Check if channel is set as ignored
@@ -96,12 +97,27 @@ sub sig_msg_pub {
 	$target = format_channel($target);
 
 	$window = Irssi::window_find_name('allwin');
-	$window->printformat(MSGLEVEL_NEVER, 'allmsg', $nick, $target, $msg, $timestamp);
+	$window->printformat(MSGLEVEL_NEVER, $format, $nick, $target, $msg, $timestamp);
+}
+
+sub sig_msg_pub {
+	my ($server, $msg, $nick, $address, $target) = @_;
+	window_output('allmsg', $nick, $target, $msg);
+}
+
+sub sig_irc_action {
+	my ($server, $msg, $nick, $address, $target) = @_;
+	window_output('allmsg_action', $nick, $target, $msg);
 }
 
 sub sig_msg_pub_own {
 	my ($server, $msg, $target) = @_;
 	sig_msg_pub($server, $msg, $server->{nick}, "", $target);
+}
+
+sub sig_irc_own_action {
+	my ($server, $msg, $target) = @_;
+	sig_irc_action($server, $msg, $server->{nick}, "", $target);
 }
 
 sub sig_window_text {
@@ -129,6 +145,6 @@ Irssi::settings_add_str('allwin', 'allwin_msg_channel', '');
 
 Irssi::signal_add('message public', 'sig_msg_pub');
 Irssi::signal_add('message own_public', 'sig_msg_pub_own');
-Irssi::signal_add('message irc own_action', 'sig_msg_pub_own');
-Irssi::signal_add('message irc action', 'sig_msg_pub');
+Irssi::signal_add('message irc own_action', 'sig_irc_own_action');
+Irssi::signal_add('message irc action', 'sig_irc_action');
 Irssi::signal_add('send text', 'sig_window_text');
