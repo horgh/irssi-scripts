75884804a417c4a4d8d17c62b88ef7d04ae533ea
Will Storey <will@summercat.com>
2016-07-01 12:06:43 -0700

    nickcolour-horgh: Assign colours to configured nicks only
    This updates the nickcolour script to no longer assign random colours to
    nicks that are not manually configured to have a colour.
    
    I modernized some of the existing code too, such as how we work with
    files.
    
    I removed code no longer needed in this version.
    
    I also improved the command help. And changed color to colour.

--- nickcolour-horgh.pl
+++ nickcolour-horgh.pl
@@ -1,145 +1,227 @@
+#
+# vim: tabstop=2:shiftwidth=2:expandtab
+#
+# This script is a copy of the nickcolor.pl script found on the Irssi scripts
+# site.
+#
+# My modification is to by default leave all nicks uncoloured. Instead, ou can
+# use it to set specific nicks to a colour.
+#
+# I like having most nicks the same colour but some (e.g. bots) having different
+# colours. I don't like the random colouring.
+#
+# I forked it from the version with this changed date:
+# Sun 15 Jun 19:10:44 BST 2014
+#
+
+use warnings;
 use strict;
+
 use Irssi 20020101.0250 ();
-use vars qw($VERSION %IRSSI); 
+use vars qw($VERSION %IRSSI);
+
 $VERSION = "2";
+
 %IRSSI = (
-    authors     => "Timo Sirainen, Ian Peters, David Leadbeater",
-    contact	=> "tss\@iki.fi", 
-    name        => "Nick Color",
-    description => "assign a different color for each nick",
-    license	=> "Public Domain",
-    url		=> "http://irssi.org/",
-    changed	=> "Sun 15 Jun 19:10:44 BST 2014",
+    authors     => "Timo Sirainen, Ian Peters, David Leadbeater, William Storey",
+    contact     => "tss\@iki.fi",
+    name        => "Nick Colour (Horgh fork)",
+    description => "Assign a different colour for each nick",
+    license     => "Public Domain",
+    url         => "https://irssi.org/",
+    changed     => "Fri  1 Jul 11:22:44 PDT 2016",
 );
 
 # Settings:
-#   nickcolor_colors: List of color codes to use.
-#   e.g. /set nickcolor_colors 2 3 4 5 6 7 9 10 11 12 13
+#   nickcolour_colours: List of colour codes to use.
+#   e.g. /set nickcolour_colours 2 3 4 5 6 7 9 10 11 12 13
 #   (avoid 8, as used for hilights in the default theme).
 
-my %saved_colors;
-my %session_colors = {};
+# %saved_colours holds those manually set.
+my %saved_colours;
 
-sub load_colors {
-  open my $color_fh, "<", "$ENV{HOME}/.irssi/saved_colors";
-  while (<$color_fh>) {
-    chomp;
-    my($nick, $color) = split ":";
-    $saved_colors{$nick} = $color;
+# %session_colours holds additional colours set. Such as temporary ones like
+# nickname changes.
+my %session_colours;
+
+sub load_colours {
+  my $fh;
+  if (!open $fh, "<", "$ENV{HOME}/.irssi/saved_colours") {
+    # Don't complain about this. There may be no saved colours.
+    return;
+  }
+
+  while (!eof $fh) {
+    my $line = <$fh>;
+    if (!defined $line) {
+      Irssi::print("Unable to read from .irssi/saved_colours");
+      close $fh;
+      return;
+    }
+
+    chomp $line;
+
+    next unless length $line > 0;
+
+    my ($nick, $colour) = split ":", $line;
+
+    next unless defined $nick && defined $colour;
+    next unless length $nick > 0;
+    next unless $colour =~ /^\d+$/;
+    next unless $colour >= 2 && $colour <= 14;
+
+    $saved_colours{ $nick } = $colour;
+  }
+
+  close $fh;
+}
+
+sub save_colours {
+  my $fh;
+  if (!open $fh, ">", "$ENV{HOME}/.irssi/saved_colours") {
+    Irssi::print("Unable to open .irssi/saved_colours: $!");
+    return;
+  }
+
+  foreach my $nick (keys %saved_colours) {
+    if (!print { $fh } "$nick:$saved_colours{$nick}\n") {
+      Irssi::print("Unable to write to .irssi/saved_colours");
+      close $fh;
+      return;
+    }
+  }
+
+  if (!close $fh) {
+    Irssi::print("Unable to close .irssi/saved_colours");
+    return;
   }
 }
 
-sub save_colors {
-  open COLORS, ">", "$ENV{HOME}/.irssi/saved_colors";
-
-  foreach my $nick (keys %saved_colors) {
-    print COLORS "$nick:$saved_colors{$nick}\n";
-  }
-
-  close COLORS;
-}
-
-# If someone we've colored (either through the saved colors, or the hash
-# function) changes their nick, we'd like to keep the same color associated
-# with them (but only in the session_colors, ie a temporary mapping).
-
+# sig_nick fires on nick changes.
+#
+# If someone we've coloured (either through the saved colours, or the hash
+# function) changes their nick, we'd like to keep the same colour associated
+# with them (but only in the session_colours, ie a temporary mapping).
 sub sig_nick {
   my ($server, $newnick, $nick, $address) = @_;
-  my $color;
+  my $colour;
 
   $newnick = substr ($newnick, 1) if ($newnick =~ /^:/);
 
-  if ($color = $saved_colors{$nick}) {
-    $session_colors{$newnick} = $color;
-  } elsif ($color = $session_colors{$nick}) {
-    $session_colors{$newnick} = $color;
+  if ($colour = $saved_colours{$nick}) {
+    $session_colours{$newnick} = $colour;
+  } elsif ($colour = $session_colours{$nick}) {
+    $session_colours{$newnick} = $colour;
   }
 }
 
-# This gave reasonable distribution values when run across
-# /usr/share/dict/words
-
-sub simple_hash {
-  my ($string) = @_;
-  chomp $string;
-  my @chars = split //, $string;
-  my $counter;
-
-  foreach my $char (@chars) {
-    $counter += ord $char;
-  }
-
-  my @colors = split / /, Irssi::settings_get_str('nickcolor_colors');
-  $counter = $colors[$counter % @colors];
-
-  return $counter;
-}
-
+# sig_public fires on public messages.
 sub sig_public {
   my ($server, $msg, $nick, $address, $target) = @_;
 
-  # Has the user assigned this nick a color?
-  my $color = $saved_colors{$nick};
+  # Has the user assigned this nick a colour?
+  my $colour;
 
-  # Have -we- already assigned this nick a color?
-  if (!$color) {
-    $color = $session_colors{$nick};
+  if (exists $saved_colours{ $nick }) {
+    $colour = $saved_colours{ $nick };
   }
 
-  # Let's assign this nick a color
-  if (!$color) {
-    $color = simple_hash $nick;
-    $session_colors{$nick} = $color;
+  if (!defined $colour && exists $session_colours{ $nick }) {
+    $colour = $session_colours{ $nick };
   }
 
-  $color = sprintf "\003%02d", $color;
-  $server->command('/^format pubmsg {pubmsgnick $2 {pubnick ' . $color . '$0}}$1');
+  if (defined $colour) {
+    $colour = sprintf "\003%02d", $colour;
+    $server->command('/^format pubmsg {pubmsgnick $2 {pubnick ' . $colour . '$0}}$1');
+    return;
+  }
+
+  # No colour set for this nick.
+  # However we need to reset the format. It is persistent if we had a format set.
+  # TODO: It seems like there should be a nicer way to do this without messing
+  #   with formats.
+  $server->command('/^format pubmsg {pubmsgnick $2 {pubnick $0}}$1');
 }
 
-sub cmd_color {
+sub cmd_colour {
   my ($data, $server, $witem) = @_;
-  my ($op, $nick, $color) = split " ", $data;
+
+  my ($op, $nick, $colour) = split " ", $data;
+
+  if (!defined $op) {
+    Irssi::print("No operation given (save/set/clear/list/preview)");
+    Irssi::print("");
+    Irssi::print("  /colour save");
+    Irssi::print("    Saves your colours to disk.");
+    Irssi::print("");
+    Irssi::print("  /colour set <nick> <colour number [2,14]>");
+    Irssi::print("    Sets a nick's colour.");
+    Irssi::print("");
+    Irssi::print("  /colour clear <nick>");
+    Irssi::print("    Clears a nick's colour.");
+    Irssi::print("");
+    Irssi::print("  /colour list");
+    Irssi::print("    List saved nick colours.");
+    Irssi::print("");
+    Irssi::print("  /colour preview");
+    Irssi::print("    List available colours.");
+    return;
+  }
 
   $op = lc $op;
 
-  if (!$op) {
-    Irssi::print ("No operation given (save/set/clear/list/preview)");
-  } elsif ($op eq "save") {
-    save_colors;
-  } elsif ($op eq "set") {
-    if (!$nick) {
-      Irssi::print ("Nick not given");
-    } elsif (!$color) {
-      Irssi::print ("Color not given");
-    } elsif ($color < 2 || $color > 14) {
-      Irssi::print ("Color must be between 2 and 14 inclusive");
-    } else {
-      $saved_colors{$nick} = $color;
-    }
-  } elsif ($op eq "clear") {
-    if (!$nick) {
-      Irssi::print ("Nick not given");
-    } else {
-      delete ($saved_colors{$nick});
-    }
-  } elsif ($op eq "list") {
-    Irssi::print ("\nSaved Colors:");
-    foreach my $nick (keys %saved_colors) {
-      Irssi::print (chr (3) . "$saved_colors{$nick}$nick" .
-		    chr (3) . "1 ($saved_colors{$nick})");
-    }
-  } elsif ($op eq "preview") {
-    Irssi::print ("\nAvailable colors:");
-    foreach my $i (2..14) {
-      Irssi::print (chr (3) . "$i" . "Color #$i");
-    }
+  if ($op eq "save") {
+    &save_colours;
+    return;
   }
+
+  if ($op eq "set") {
+    if (!$nick) {
+      Irssi::print("Nick not given");
+    } elsif (!defined $colour) {
+      Irssi::print("Colour not given");
+    } elsif ($colour < 2 || $colour > 14) {
+      Irssi::print("Colour must be between 2 and 14 inclusive");
+    } else {
+      $saved_colours{$nick} = $colour;
+      &save_colours;
+    }
+    return;
+  }
+
+  if ($op eq "clear") {
+    if (!$nick) {
+      Irssi::print("Nick not given");
+    } else {
+      delete ($saved_colours{$nick});
+    }
+    return;
+  }
+
+  if ($op eq "list") {
+    Irssi::print("\nSaved colours:");
+    foreach my $nick (keys %saved_colours) {
+      Irssi::print(chr (3) . "$saved_colours{$nick}$nick" .
+        chr (3) . "1 ($saved_colours{$nick})");
+    }
+    return;
+  }
+
+  if ($op eq "preview") {
+    Irssi::print("\nAvailable colours:");
+    foreach my $i (2..14) {
+      Irssi::print(chr (3) . "$i" . "colour #$i");
+    }
+    return;
+  }
+
+  Irssi::print("Unknown operation given. Use one of save/set/clear/list/preview.");
 }
 
-load_colors;
+&load_colours;
 
-Irssi::settings_add_str('misc', 'nickcolor_colors', '2 3 4 5 6 7 9 10 11 12 13');
-Irssi::command_bind('color', 'cmd_color');
+Irssi::settings_add_str('misc', 'nickcolour_colours', '2 3 4 5 6 7 9 10 11 12 13');
+Irssi::command_bind('colour', 'cmd_colour');
 
 Irssi::signal_add('message public', 'sig_public');
 Irssi::signal_add('event nick', 'sig_nick');
